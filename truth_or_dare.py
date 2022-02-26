935f7950092fd1ee72e541ddf588387553b580ed
Shiv <tomedashiva@yahoo.in>
2022-02-26 23:10:38 +0530

    Update truth_or_dare.py
    added pep 8 variable declaraions

--- truth_or_dare.py
+++ truth_or_dare.py
@@ -1,3 +1,18 @@
+'''
+Variable Naming convention start point
+'''
+
+
+name = "shiva" # lower case single letter
+first_name = "Shiva" # lower case two letters with _  only these two are good practices as per PEP8.
+
+user = "shiva"
+user_info = {"name":"Shiva","age":30} # always use double quotes
+
+'''
+Variable Naming convention End point
+'''
+
 a = ''
 
 if a =='':  #bad 
